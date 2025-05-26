5f3392397ce06c64b6dee69c89a946c1cf951c6f
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-05-26 08:00:26 +0530

    fibonacci series

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // wap to print sum of even and odd numbers in a range
 
-var start_num = prompt(Number("Enter the starting number : "))
-var end_num = prompt(Number("Enter the ending number : "))
+var start_num = Number(prompt("Enter the starting number : "))
+var end_num = Number(prompt("Enter the ending number : "))
 
 var even_sum = 0
 var odd_sum = 0
@@ -17,7 +17,7 @@ for (var i = start_num; i<=end_num; i++){
 
 // factorial of any number
 
-var fact = prompt(Number("Enter the factorial number : "))
+var fact = Number(prompt("Enter the factorial number : "))
 
 var factorial = 1
 
@@ -28,7 +28,7 @@ for (var i=fact; i>=1; i--){
 
 // perfect number
 
-var perfect_num = prompt(Number("Enter the number to check whether perfect or not : "))
+var perfect_num = Number(prompt("Enter the number to check whether perfect or not : "))
 var divisor_sum = 0
 for (var divisor = 1; divisor < perfect_num; divisor++){
     if (perfect_num%divisor==0){
@@ -43,4 +43,41 @@ else {
     console.log("Not a perfect sum");
 }
 
-//
+// prime or not
+
+var prime_num = Number(Prompt("Enter the number to check prime or not : "))
+var prime_flag = false
+
+for (var i=2; i<prime_num**0.5; i++){
+    if (prime_num%i==0){
+        prime_flag = true
+    }
+}
+
+if (prime_flag == false){
+    console.log("prime number");
+}
+else{
+    console.log("not a prime number");
+}
+
+//fibonacci series
+
+var fibonacci_num = Number(prompt("Enter the fibonacci number : "))
+
+var fib_first = 0
+var fib_second = 1
+var fib_sum = fib_first + fib_second
+
+document.write(`${fib_first} ${fib_second}`)
+
+while (fib_sum<= fibonacci_num){
+    document.write(`${fib_sum}`)
+    fib_first = fib_second
+    fib_second = fib_sum
+    fib_sum = fib_first + fib_second
+}
+
+// sum of number of digits
+
+var sum_num  = Number(prompt("Enter the number : "))
